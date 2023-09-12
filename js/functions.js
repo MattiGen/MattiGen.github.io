1d049f39f7a6efc40d4f42fa36dc38cc4506dfb5
Matti Gencher <meg.g@live.com>
2023-09-12 17:19:48 -0400

    Add capstone page

--- js/functions.js
+++ js/functions.js
@@ -27,3 +27,36 @@ function listProjs() {
         }
     }
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("fydp1");
+var img2 = document.getElementById("fydp2");
+var img3 = document.getElementById("fydp3")
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+img2.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//TODO: Clean this code, allow me to cycle through left and right
--- readme.md
+++ readme.md
@@ -1,16 +1,13 @@
 ## Project Description
 
-* [live example](https://learning-zone.github.io/website-templates/3-col-portfolio)
-
-![alt text](https://github.com/learning-zone/website-templates/blob/master/assets/3-col-portfolio.png "3-col-portfolio.png")
-
-Thanks to Pradeep Kumar at https://github.com/learning-zone for the template
+* [Site](https://mattigen.github.io)
 
 
 To do list
-[] About me page
+[x] About me page
 [x] Button to load resume/portfolio pdf
 [] Projects page (rewrite portfolio with some embedded videos)
-[] Course work page
-[] Have another dropdown with all the projects, and have a quick summary on the home/about me page
+[x] Course work page
+[x] Have another dropdown with all the projects, and have a quick summary on the home/about me page
 [] Make urls not have .html at the end
+[] Listen to linter and clean up the code
