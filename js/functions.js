cc35f356bb338fa720d6aa775c94ffe8d4ae173a
Matti Gencher <meg.g@live.com>
2023-09-11 20:11:51 -0400

    Upload first rev of site

---
+++ js/functions.js
@@ -0,0 +1,29 @@
+/* When the user clicks on the button, 
+      toggle between hiding and showing the dropdown content */
+function listDocs() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    }
+      
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+
+function listProjs() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+    }
+      
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn2')) {
+        var myDropdown = document.getElementById("myDropdown2");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+## Project Description
+
+* [live example](https://learning-zone.github.io/website-templates/3-col-portfolio)
+
+![alt text](https://github.com/learning-zone/website-templates/blob/master/assets/3-col-portfolio.png "3-col-portfolio.png")
+
+Thanks to Pradeep Kumar at https://github.com/learning-zone for the template
+
+
+To do list
+[] About me page
+[x] Button to load resume/portfolio pdf
+[] Projects page (rewrite portfolio with some embedded videos)
+[] Course work page
+[] Have another dropdown with all the projects, and have a quick summary on the home/about me page
+[] Make urls not have .html at the end
